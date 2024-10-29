90185fbe20e62001e1df4e0277f8549456a5a825
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-10-29 19:59:46 -0400

    score_titulo prueba

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
+popularity_test = pd.read_csv('popularity_test.csv')
 
 @app.get("/")
 async def root():
@@ -21,3 +22,9 @@ async def prueba():
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+@app.get("/score_titulo/")
+async def score_titulo(title: str = ''):
+    indice = popularity_test.index[popularity_test['title']== title][0]
+    return print(f"La película {popularity_test['title'][indice]} fue estrenada en el año {popularity_test['release_year'][indice]} con un score/popularidad de {popularity_test['popularity'][indice]}")
+    
