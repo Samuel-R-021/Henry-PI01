806b73cbe5d6939b3c5bfeda39728df3be3b50df
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-10-29 21:07:37 -0400

    funcion score_titulo lista

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 popularity_test = pd.read_csv('popularity_test.csv')
 
-@app.get("/score_titulo")
+@app.get("/score_titulo/")
 async def score_titulo(title: str = ''):
     indice = popularity_test.index[popularity_test['title']== title][0]
     return f"La película {popularity_test['title'][indice]} fue estrenada en el año {popularity_test['release_year'][indice]} con un score/popularidad de {popularity_test['popularity'][indice]}"
