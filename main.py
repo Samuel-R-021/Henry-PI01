229dd8a46155d0ff0aa98c5ee1b6d2031ea84daf
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-10-29 20:47:03 -0400

    removiendo print()

--- main.py
+++ main.py
@@ -26,5 +26,5 @@ async def read_item(skip: int = 0, limit: int = 10):
 @app.get("/score_titulo/")
 async def score_titulo(title: str = ''):
     indice = popularity_test.index[popularity_test['title']== title][0]
-    return print(f"La película {popularity_test['title'][indice]} fue estrenada en el año {popularity_test['release_year'][indice]} con un score/popularidad de {popularity_test['popularity'][indice]}")
+    return f"La película {popularity_test['title'][indice]} fue estrenada en el año {popularity_test['release_year'][indice]} con un score/popularidad de {popularity_test['popularity'][indice]}"
     
