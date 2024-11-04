0c37a2d6dd1e4f346105cb7655f5a60b1061fe87
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-11-03 21:43:57 -0400

    creando datasets para func_1_4

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 app = FastAPI()
 
-func_dataset = pd.read_csv('full_funciones_dataset.csv', index_col='lowercase_title')
+func_1_4_dataset = pd.read_csv('funciones_1_4_dataset.csv', index_col='lowercase_title')
 
 @app.get("/cantidad_filmaciones_mes/")
 async def cantidad_filmaciones_mes(mes: str = ''):
-    total = func_dataset['month_name'][func_dataset['month_name']== mes.capitalize()].count()
+    total = func_1_4_dataset['month_name'][func_1_4_dataset['month_name']== mes.capitalize()].count()
     return f'{total} películas fueron estrenadas en el mes de {mes.lower()}'
 
 @app.get("/cantidad_filmaciones_dia/")
@@ -16,15 +16,15 @@ async def cantidad_filmaciones_dia(dia: str = ''):
         dia = 'miércoles'
     elif dia.lower() == 'sabado':
         dia = 'sábado'
-    total = func_dataset['day_name'][func_dataset['day_name']== dia.capitalize()].count()
+    total = func_1_4_dataset['day_name'][func_1_4_dataset['day_name']== dia.capitalize()].count()
     return f'{total} películas fueron estrenadas en los días {dia.lower()}'
 
 @app.get("/score_titulo/")
 async def score_titulo(title: str = ''):
     indice = title.lower()
-    return f"La película {func_dataset['title'][indice]} fue estrenada en el año {func_dataset['release_year'][indice]} con un score/popularidad de {func_dataset['popularity'][indice]}"
+    return f"La película {func_1_4_dataset['title'][indice]} fue estrenada en el año {func_1_4_dataset['release_year'][indice]} con un score/popularidad de {func_1_4_dataset['popularity'][indice]}"
     
 @app.get("/votos_titulo/")
 async def votos_titulo(title: str = ''):
     indice = title.lower()
-    return f"La película {func_dataset['title'][indice]} no tiene al menos 2000 valoraciones, por lo que no se devuelve ningun valor." if func_dataset['vote_count'][indice] < 2000 else f"La película {func_dataset['title'][indice]} fue estrenada en el año {func_dataset['release_year'][indice]}. La misma cuenta con un total de {int(func_dataset['vote_count'][indice])} valoraciones, con un promedio de {func_dataset['vote_average'][indice]}"
+    return f"La película {func_1_4_dataset['title'][indice]} no tiene al menos 2000 valoraciones, por lo que no se devuelve ningun valor." if func_1_4_dataset['vote_count'][indice] < 2000 else f"La película {func_1_4_dataset['title'][indice]} fue estrenada en el año {func_1_4_dataset['release_year'][indice]}. La misma cuenta con un total de {int(func_1_4_dataset['vote_count'][indice])} valoraciones, con un promedio de {func_1_4_dataset['vote_average'][indice]}"
