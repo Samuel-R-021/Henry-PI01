c52174b5dea57b6eeff638acbc8ea7b775ca3831
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-11-05 16:05:24 -0400

    recomendacion max_features=1,250

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ cast_dataset = pd.read_csv('cast_credits.csv',index_col='lowercase_name')
 crew_dataset = pd.read_csv('crew_credits.csv',index_col='lowercase_name')
 modelo_dataset = pd.read_csv('modelo_database.csv')
 
-cv = CountVectorizer(max_features=500, stop_words='english')
+cv = CountVectorizer(max_features=1250, stop_words='english')
 vector = cv.fit_transform(modelo_dataset['tags']).toarray()
 similitud = cosine_similarity(vector)
 
