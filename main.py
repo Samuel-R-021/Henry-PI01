c861bee2ee5d9cb822dd6c77e09a46df6275a9e1
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-11-05 17:59:32 -0400

    main ajustes

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ cast_dataset = pd.read_csv('cast_credits.csv',index_col='lowercase_name')
 crew_dataset = pd.read_csv('crew_credits.csv',index_col='lowercase_name')
 modelo_dataset = pd.read_csv('modelo_database.csv')
 
-cv = CountVectorizer(max_features=1250, stop_words='english')
-vector = cv.fit_transform(modelo_dataset['tags']).toarray()
-similitud = cosine_similarity(vector)
+# cv = CountVectorizer(max_features=1250, stop_words='english')
+# vector = cv.fit_transform(modelo_dataset['tags']).toarray()
+# similitud = cosine_similarity(vector)
+
+@app.get("/")
+async def root():
+    return {"message": "Proyecto Individual 1 DataScience, Samuel Rangel DataPT11"}
 
 @app.get("/cantidad_filmaciones_mes/")
 async def cantidad_filmaciones_mes(mes: str = ''):
+    if mes.capitalize() not in {'Enero','Febrero', 'Marzo','Abril','Mayo', 'Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'}:
+        return 'Por favor ingrese un mes valido en español'
     total = func_1_4_dataset['month_name'][func_1_4_dataset['month_name']== mes.capitalize()].count()
     return f'{total} películas fueron estrenadas en el mes de {mes.lower()}'
 
@@ -26,6 +32,9 @@ async def cantidad_filmaciones_dia(dia: str = ''):
         dia = 'miércoles'
     elif dia.lower() == 'sabado':
         dia = 'sábado'
+    if dia.capitalize() not in {'Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'}:
+        return 'Por favor ingrese un dia de la semana valido en español'
+    
     total = func_1_4_dataset['day_name'][func_1_4_dataset['day_name']== dia.capitalize()].count()
     return f'{total} películas fueron estrenadas en los días {dia.lower()}'
 
@@ -44,12 +53,10 @@ async def get_actor(nombre_actor: str = ''):
     
     # convertir a minuscula para evitar errores, y usar como indice en el cast_dataset
     indice = nombre_actor.lower()
-    
 
     # aqui tomamos los id sacados de cast_dataset y buscamos cuales se encuentran en el dataset de movies (func_5_6_dataset)
     # y nos quedamos solo con los que SI estan en el datasets de movies.
     movies_cast_id_match = func_5_6_dataset['id'].isin(cast_dataset['id'][indice])
-
     # valores a extraer para el resultado de la funcion
     nombre = cast_dataset['name'][indice][0]
     filmaciones = func_5_6_dataset['id'][movies_cast_id_match].shape[0]
@@ -62,7 +69,6 @@ async def get_director(nombre_actor: str = ''):
     indice = nombre_actor.lower()
     
     movies_crew_id_match = func_5_6_dataset['id'].isin(crew_dataset['id'][indice])
-
     nombre = crew_dataset['name'][indice][0]
     filmaciones = func_5_6_dataset['id'][movies_crew_id_match]
     retorno = func_5_6_dataset['return'][movies_crew_id_match].sum()
@@ -72,14 +78,14 @@ async def get_director(nombre_actor: str = ''):
 @app.get("/recomendacion/")
 async def recomendacion(titulo: str = ''):
     
-    # cv = CountVectorizer(max_features=5000, stop_words='english')
-    # vector = cv.fit_transform(modelo_dataset['tags']).toarray()
-    # similitud = cosine_similarity(vector)
+    cv = CountVectorizer(max_features=1250, stop_words='english')
+    vector = cv.fit_transform(modelo_dataset['tags']).toarray()
+    similitud = cosine_similarity(vector)
 
     indice = modelo_dataset[modelo_dataset['title']== titulo].index[0]
     distancia = sorted(list(enumerate(similitud[indice])), reverse=True, key=lambda x: x[1])
 
-    for i in distancia[1:6]:
-        print(modelo_dataset.iloc[i[0]].title)
+    # for i in distancia[1:6]:
+    #     print(modelo_dataset.iloc[i[0]].title)
 
-    return [modelo_dataset.iloc[i[0]].title for i in distancia[1:6]]
+    return [modelo_dataset['title'][i[0]] for i in distancia[1:6]]
