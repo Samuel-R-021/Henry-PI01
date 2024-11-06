c89e1f0166936834cf206babb855c11acdb68c05
Samuel Rangel <samuel.rangel.021@gmail.com>
2024-11-05 23:03:11 -0400

    update main comentarios

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ from fastapi import FastAPI
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# donde esta index_col=, se hizo para escoger dicha columna como indice para simplificar las correspondientes busquedas
 func_1_4_dataset = pd.read_csv('funciones_1_4_dataset.csv', index_col='lowercase_title')
 func_5_6_dataset = pd.read_csv('funciones_5_6_dataset.csv')
-cast_dataset = pd.read_csv('cast_credits.csv',index_col='lowercase_name')
+cast_dataset = pd.read_csv('cast_credits.csv',index_col='lowercase_name') 
 crew_dataset = pd.read_csv('crew_credits.csv',index_col='lowercase_name')
 modelo_dataset = pd.read_csv('modelo_database.csv')
+
+class Resumen(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+class Exito_Individual(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 @app.get("/")
 async def root():
@@ -17,50 +31,68 @@ async def root():
 
 @app.get("/cantidad_filmaciones_mes/")
 async def cantidad_filmaciones_mes(mes: str = ''):
+    
+    # En el dataset todos los meses inician con mayuscula, por ello se usa .capitalize(), para garantizar el match.
+
     if mes.capitalize() not in {'Enero','Febrero', 'Marzo','Abril','Mayo', 'Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'}:
         return 'Por favor ingrese un mes valido en español'
+    # Se extrae un dataframe mas pequeño donde los meses son igual al input, y se cuenta el numero de filas
     total = func_1_4_dataset['month_name'][func_1_4_dataset['month_name']== mes.capitalize()].count()
     return f'{total} películas fueron estrenadas en el mes de {mes.lower()}'
 
 @app.get("/cantidad_filmaciones_dia/")
 async def cantidad_filmaciones_dia(dia: str = ''):
+
+    # Se acepta los inputs aun si no tienen acentos correctamente.
+
     if dia.lower() == 'miercoles':
         dia = 'miércoles'
     elif dia.lower() == 'sabado':
         dia = 'sábado'
+
+    # En el dataset los dias de la semana inician con mayuscula, por ello se usa .capitalize(), para garantizar el match.
+
     if dia.capitalize() not in {'Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'}:
         return 'Por favor ingrese un dia de la semana valido en español'
     
+    # Se extrae un dataframe mas pequeño donde los dias son igual al input, y se cuenta el numero de filas    
     total = func_1_4_dataset['day_name'][func_1_4_dataset['day_name']== dia.capitalize()].count()
     return f'{total} películas fueron estrenadas en los días {dia.lower()}'
 
 @app.get("/score_titulo/")
 async def score_titulo(titulo_de_la_filmacion: str = ''):
+
+    # Para buscar la pelicula, al abrir el dataset, se coloca la columna de titulo como indice, para facilitar la busqueda.
     indice = titulo_de_la_filmacion.lower()
     if indice not in func_1_4_dataset.index:
-       return 'La pelicula no esta en la base de datos'
+       return 'Hay algun error en el nombre introducido (considere mayúsculas) o la película no se encuentra en la base de datos'
     return f"La película {func_1_4_dataset['title'][indice]} fue estrenada en el año {func_1_4_dataset['release_year'][indice]} con un score/popularidad de {func_1_4_dataset['popularity'][indice]}"
     
 @app.get("/votos_titulo/")
 async def votos_titulo(titulo_de_la_filmacion: str = ''):
+
+    # Para buscar la pelicula, al abrir el dataset, se coloca la columna de titulo como indice, para facilitar la busqueda.
     indice = titulo_de_la_filmacion.lower()
     if indice not in func_1_4_dataset.index:
-       return 'La pelicula no esta en la base de datos'
+       return 'Hay algun error en el nombre introducido (considere mayúsculas) o la película no se encuentra en la base de datos'
     return f"La película {func_1_4_dataset['title'][indice]} tiene menos de 2000 valoraciones, por lo que no se devuelve ningun valor." if func_1_4_dataset['vote_count'][indice] < 2000 else f"La película {func_1_4_dataset['title'][indice]} fue estrenada en el año {func_1_4_dataset['release_year'][indice]}. La misma cuenta con un total de {int(func_1_4_dataset['vote_count'][indice])} valoraciones, con un promedio de {func_1_4_dataset['vote_average'][indice]}"
 
 @app.get("/get_actor/")
 async def get_actor(nombre_actor: str = ''):
     
-    # convertir a minuscula para evitar errores, y usar como indice en el cast_dataset
+    # Se convierte a minuscula para evitar errores, y usar como indice en el cast_dataset
     indice = nombre_actor.lower()
 
+    # se chequea si el actor aparece como indice en el dataset, de no ser asi regresa el mensaje de error
     if indice not in cast_dataset.index:
-       return 'El actor no esta en la base de datos'
-    # aqui tomamos los id sacados de cast_dataset y buscamos cuales se encuentran en el dataset de movies (func_5_6_dataset)
-    # y nos quedamos solo con los que SI estan en el datasets de movies.
+       return 'Hay algun error en el nombre introducido o el actor no esta en la base de datos'
+    
+    # Si el actor esta en el dataset, se obtienen las peliculas en cast_dataset, y luego se compara con
+    # el dataset de movies (func_5_6_dataset) y nos quedamos solo con aquellos donde el id de la pelicula
+    # aparezca en ambas
     movies_cast_id_match = func_5_6_dataset['id'].isin(cast_dataset['id'][indice])
     
-    # valores a extraer para el resultado de la funcion
+    # valores a extraer de ambos datasets para el resultado de la funcion
     nombre = cast_dataset['name'][indice][0]
     filmaciones = func_5_6_dataset['id'][movies_cast_id_match].shape[0]
     retorno = func_5_6_dataset['return'][movies_cast_id_match].sum()
@@ -69,10 +101,20 @@ async def get_actor(nombre_actor: str = ''):
 
 @app.get("/get_director/")
 async def get_director(nombre_actor: str = ''):
+
+    # Se convierte a minuscula para evitar errores, y usar como indice en el crew_dataset
     indice = nombre_actor.lower()
+
+    # se chequea si el actor aparece como indice en el dataset, de no ser asi regresa el mensaje de error
     if indice not in crew_dataset.index:
-       return 'El director no esta en la base de datos'
+       return 'Hay algun error en el nombre introducido o el director no esta en la base de datos'
+    
+    # Si el director esta en el dataset, se obtienen las peliculas en crew_dataset, y luego se compara con
+    # el dataset de movies (func_5_6_dataset) y nos quedamos solo con aquellos donde el id de la pelicula
+    # aparezca en ambas
     movies_crew_id_match = func_5_6_dataset['id'].isin(crew_dataset['id'][indice])
+
+    # valores a extraer de ambos datasets para el resultado de la funcion
     nombre = crew_dataset['name'][indice][0]
     filmaciones = func_5_6_dataset['id'][movies_crew_id_match]
     retorno = func_5_6_dataset['return'][movies_crew_id_match].sum()
@@ -81,14 +123,26 @@ async def get_director(nombre_actor: str = ''):
 
 @app.get("/recomendacion/")
 async def recomendacion(titulo: str = ''):
+
+    # Se chequea que la pelicula esta en el dataset, de no ser asi regresa mensaje de error
     if titulo not in modelo_dataset['title'].tolist():
-        return f'Hay algun error en el nombre introducido ({titulo}, considere mayúsculas) o la película no se encuentra en la base de datos'
-        
+        return 'Hay algun error en el nombre introducido (considere mayúsculas) o la película no se encuentra en la base de datos'
+    
+    # instanciar el vectorizador para fijar los parametros, en este caso limite de atributos (palabras) y remover palabras no relevantes
     cv = CountVectorizer(max_features=1250, stop_words='english')
+
+    # la transformacion hace que tengamos tantas columnas como palabras diferentes y ver cuales aparecen
+    # aqui la cantidad de palabras diferentes son 1250 pq fue el limite puesto al vectorizador
     vector = cv.fit_transform(modelo_dataset['tags']).toarray()
+
+    # la funcion calcula la semejanza entre cada registro con el resto de los registros
     similitud = cosine_similarity(vector)
 
+    # se busca el indice correspondiente a la pelicula que se ingreso
     indice = modelo_dataset[modelo_dataset['title']== titulo].index[0]
+
+    # con el indice se extrae la fila de la pelicula y toda su semejanza con el resto de peliculas, ordenando de mayor a menor
     distancia = sorted(list(enumerate(similitud[indice])), reverse=True, key=lambda x: x[1])
 
+    # se extrae del 2do al 6 valor (el primero es la pelicula ingresada)
     return [modelo_dataset['title'][i[0]] for i in distancia[1:6]]
